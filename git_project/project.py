8ec8cdf2573565e4349fc33c236e99edac74aade
David A. Greene <greened@obbligato.org>
2020-11-13 21:10:35 -0600

    [Branch] Handle missing remote branch
    A local branch might not have a remote push target.  If it doesn't just skip
    that remote.

--- git_project/project.py
+++ git_project/project.py
@@ -164,6 +164,8 @@ class Project(ScopedConfigObject):
             target_refname = self._git.committish_to_refname(target)
             for remote in self.iterremotes():
                 target_remote_refname = self._git.get_remote_push_refname(target_refname, remote)
+                if not target_remote_refname:
+                    continue
                 if self._git.refname_is_merged(refname, target_remote_refname):
                     return True
         return False
