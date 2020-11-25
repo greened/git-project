47a4453324aa150d38012f7a2b4d0f188f416be7
David A. Greene <dag@cray.com>
2020-11-25 10:31:30 -0600

    [Packaging] Version bump
    Bump to 0.0.2.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
         return f.read()
 
 setup(name='git_project',
-      version='0.0.1',
+      version='0.0.2',
       description='Manage projects in a git repository',
       long_description=readme(),
       classifiers=[
