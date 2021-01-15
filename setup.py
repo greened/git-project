518700a3944093e454187827cf7920bbf15f2175
David A. Greene <greened@obbligato.org>
2021-01-14 20:52:00 -0600

    [Packaging] Version bump
    Release version 0.0.11.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
         return f.read()
 
 setup(name='git_project',
-      version='0.0.10',
+      version='0.0.11',
       description='Manage projects in a git repository',
       long_description=readme(),
       classifiers=[
