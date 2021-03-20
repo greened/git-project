4f9c8845d6d4da0c86266462519a1bcb6e873aa7
David Greene <dag@obbligato.org>
2021-03-19 19:15:42 -0500

    [Substitutable] Support gitdir substitution
    Add {gitdir} as a substitution template to be replaced by GITDIR.

--- tests/test_substitutable.py
+++ tests/test_substitutable.py
@@ -237,3 +237,28 @@ def test_substitutable_substitute_formats(reset_directory, git):
                                              formats)
 
     assert command == f'cd {project.builddir}/{git.get_current_branch()} && make debug opt'
+
+def test_substitutable_substitute_gitdir(reset_directory, git):
+    class MyProject(git_project.ScopedConfigObject):
+        def __init__(self):
+            super().__init__(git,
+                             'project',
+                             None,
+                             'myproject',
+                             builddir='{gitdir}/../../path/to/build',
+                             target='debug {options}')
+
+    substitutable = MySubstitutable.get(git, 'project', 'test')
+
+    project = MyProject()
+
+    formats = {
+        'options': 'opt'
+    }
+
+    command = substitutable.substitute_value(git,
+                                             project,
+                                             substitutable.command,
+                                             formats)
+
+    assert command == f'cd {git.get_gitdir()}/../../path/to/build/{git.get_current_branch()} && make debug opt'
